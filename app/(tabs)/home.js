286878d6ec3355dd9e12907c9b9ad2bd17e07eb8
urvisha <urvisham.octal8@gmail.com>
2025-12-04 18:56:19 +0530

    icon chat

--- app/(tabs)/home.js
+++ app/(tabs)/home.js
@@ -12,6 +12,7 @@ import { useRouter } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Ionicons } from "@expo/vector-icons";
 import Feather from "@expo/vector-icons/Feather";
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import React, { useState } from "react";
 
 import logo1 from "@/assets/logo1.jpeg";
@@ -22,16 +23,16 @@ import logo5 from "@/assets/logo5.jpeg";
 import logo6 from "@/assets/logo6.jpeg";
 import logo7 from "@/assets/logo7.jpeg";
 
-
 export default function Home() {
   const router = useRouter();
   const [search, setSearch] = useState("");
+  
 
   const logout = async () => {
     await AsyncStorage.removeItem("token");
     router.replace("/login");
   };
-
+  const options = ["Option 1", "Option 2", "Option 3"];
   const Data = [
     {
       id: 1,
@@ -88,21 +89,23 @@ export default function Home() {
     <SafeAreaView className="flex-1 bg-white">
       <View className="bg-white pt-10 ">
         <View className="flex-row justify-between">
-          <Text className="font-bold text-2xl text-green-500 pl-2">
+          <Text className="font-bold text-2xl text-green-500 pl-4">
             WhatsApp
           </Text>
           <View className="flex-row">
             <View className="pr-2 pt-2">
               <Feather name="camera" size={24} color="black" />
             </View>
+           
             <TouchableOpacity
-              className="bg-gray-300 p-3 rounded-3xl pl-4"
+              className=" mt-2 mr-3"
               onPress={logout}
             >
-              <Text className="text-gray-800">Logout</Text>
+            <MaterialCommunityIcons name="dots-vertical" size={24} color="black" />
             </TouchableOpacity>
-          </View>
+           </View>
         </View>
+        
         <View className="flex-row items-center bg-gray-200 mx-4 my-4 px-3  rounded-2xl mt-2">
           <Ionicons name="search-outline" size={22} color="gray" />
 
@@ -119,24 +122,28 @@ export default function Home() {
             </TouchableOpacity>
           )}
         </View>
-        <View className="flex-row mx-4">
-          <TouchableOpacity className="bg-gray-200 p-3 rounded-3xl pl-4">
+        <View className="flex-row mx-4 ">
+          <TouchableOpacity className="bg-gray-200 p-3 rounded-3xl pl-4 mr-2">
             <Text className="text-gray-700">All</Text>
           </TouchableOpacity>
-          <TouchableOpacity className="bg-gray-200 p-3 rounded-3xl pl-4">
+          <TouchableOpacity className="bg-gray-200 p-3 rounded-3xl pl-4 mr-2">
             <Text className="text-gray-700">Unread</Text>
           </TouchableOpacity>
-          <TouchableOpacity className="bg-gray-200 p-3 rounded-3xl pl-4">
+          <TouchableOpacity className="bg-gray-200 p-3 rounded-3xl pl-4 mr-2">
             <Text className="text-gray-700">Favourites</Text>
           </TouchableOpacity>
-          <TouchableOpacity className="bg-gray-200 p-3 rounded-3xl pl-4">
+          <TouchableOpacity className="bg-gray-200 p-3 rounded-3xl pl-4 mr-2">
             <Text className="text-gray-700 ">Groups</Text>
           </TouchableOpacity>
           <TouchableOpacity className="bg-gray-200 p-3  rounded-3xl pl-4">
             <Text className="text-gray-700 text-bold ">+</Text>
           </TouchableOpacity>
         </View>
+       
+        
       </View>
+      
+      
       <ScrollView contentContainerClassName="mx-4">
         <View>
           <FlatList
@@ -172,6 +179,9 @@ export default function Home() {
                     </View>
                   </TouchableOpacity>
                 </View>
+
+
+
               );
             }}
           />
